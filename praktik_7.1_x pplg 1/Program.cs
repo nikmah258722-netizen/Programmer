1716e07bbc0dffc838e5957833fd944cb52ad837
nikmah258722-netizen <nikmah258722@student.smkn1kandeman.sch.id>
2025-11-24 11:07:40 +0700

    Add files via upload

---
+++ praktik_7.1_x pplg 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_7._1_x_pplg_1
+{
+    internal class Program
+    {
+        static void Sapa()
+        {
+            Console.WriteLine("Halo, selamat datang di pembelajaran C#!");
+            Console.WriteLine("Mari belajar tentang fungsi!");
+        }
+
+
+        static void Main(string[] args)
+        {
+            Sapa(); // memanggil fungsi
+        }
+
+    }
+}
---
+++ praktik_7.2_x pplg 1/praktik_7.2_x pplg 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_7._2_x_pplg_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
